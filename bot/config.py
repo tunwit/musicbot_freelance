5a1ceee4605a38d661126a88e71f88ea4adcf79c
tunwit <tunwit2458@gmail.com>
2024-06-03 22:28:55 +0700

    set database Primary key to auto increment

--- bot/config.py
+++ bot/config.py
@@ -8,7 +8,7 @@ logger = logging.getLogger('littlebirdd')
 
 path = os.getcwd()
 parent = os.path.abspath(os.path.join(path, os.pardir))
-print(parent)
+
 with open(f"{parent}\\_config.json", "r") as f:
     config = json.load(f)
 
@@ -19,9 +19,9 @@ logger.info('Load new .env.development')
 connection = sqlite3.connect(f'{parent}\\bot\\data\\bot_data.db')
 connection.execute(''' 
         CREATE TABLE IF NOT EXISTS search_history (
-        id      INT PRIMARY KEY  ,
+        id      INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
         music   TEXT ,
-        times   INT 
+        times   INTEGER 
         );
         ''')
 connection.commit()
